2afb92db74c5524aa1ebb3de2bfbb0a86709ce7f
jmobley62 <jaquan_mobley@yahoo.com>
2022-06-08 23:55:14 -0400

    test 2

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Script from 'next/script'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div class='bg-black w-screen h-screen text-red-900'>
       <title>Apex Stats | Home</title>
-      <div>Home ofo Valk</div>
+      <div>Home</div>
     </div>
   )
 }
