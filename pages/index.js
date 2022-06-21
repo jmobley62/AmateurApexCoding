85b9a35541ad96cafceb7a0a23244164dce7f9e5
jmobley62 <jaquan_mobley@yahoo.com>
2022-06-20 20:22:47 -0400

    first name, last name

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,20 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div class='bg-black w-screen h-screen text-red-900'>
+    <div class='bg-black w-screen h-screen text-red-900 px-4 py-2'>
       <title>Apex Stats | Home</title>
+
+      <form class='container w-full l-full px-36 py-4'> 
+        <label class='text-2xl font-bold px-7'>First name:  </label>
+        <input class='mt-2'></input><br></br>
+        <label class='text-2xl font-bold px-7'>Last name:  </label>
+        <input class='mt-2'></input>
+      </form>
+
       <div>Home</div>
+
     </div>
   )
 }
+
+console.log('hello world')
