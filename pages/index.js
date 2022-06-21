0b90767b922be2013a020696fe2bc4459522c458
jmobley62 <jaquan_mobley@yahoo.com>
2022-06-20 21:04:33 -0400

    V3

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div class='bg-black w-screen h-screen text-red-900 px-4 py-2'>
+    <div class='bg-black w-screen h-screen text-red-900'>
       <title>Apex Stats | Home</title>
 
-      <form class='container w-full l-full px-36 py-4'> 
+      <form class='container w-full l-full px-40 py-4'> 
         <label class='text-2xl font-bold px-7'>First name:  </label>
         <input class='mt-2'></input><br></br>
         <label class='text-2xl font-bold px-7'>Last name:  </label>
@@ -16,7 +16,9 @@ export default function Home() {
         <button class='bg-red-900 rounded-md text-white text-lg mx-14 mt-4 p-1 '>Submit</button>
       </form>
 
-      <div>Home</div>
+      <footer class='container w-full l-full flex justify-center items-center text-white bg-red-900 fixed bottom-0'>
+          <div>Copyright to JaQuan Mobley 2022<br></br>All rights reserved</div>
+        </footer>
 
     </div>
   )
